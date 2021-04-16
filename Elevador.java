d41fa4e8f44a2cd5cf5355c9733ad4e2117af034
William Distler Neves <>
2021-04-16 02:51:58 +0000

    continuação

--- Elevador.java
+++ Elevador.java
@@ -6,42 +6,58 @@ class Elevador {
   private int totalRun; //historico deslocamento
 
   public void inicializar(String codigo) {
-    if (estaDesligado()) {
+    if (this.estaDesligado()) {
       this.codigo = codigo;
       this.totalRun = 0;
       this.estado = 1;
 
     } else {
-      System.out.println("(log) falha!");
+      System.out.println("(log) falha ao inicializar!");
     }
   }
 
   public void ligar() {
-
+    if(this.estado == 0)
+      this.estado = 1;
+    else
+      System.out.println("(log) falha, no ligar!");
   }
 
   public void desligar() {
-
+    if(this.estado == 1 && this.posicao == 0) //ligado e esta no terreo
+      this.estado = 0;
+    else
+      System.out.println("(log) falha, ao desligar!");
   }
 
   public void mover(int destino) {
+    if(destino >= 0 && estado ==1){
+      int distancia;
+      if(destino > posicao){
+        System.out.println("(log) subindo ...!");
+        distancia = destino - posicao;
+      } else {
+        System.out.println("(log) descendo ...!");
+        distancia = posicao - destino;
+      }
 
+      totalRun = this.totalRun + distancia;
+      this.posicao = destino;
+    } else {
+      System.out.println("(log) falha, no mover!");
+    }
   }
 
   public String getCodigo() {
-    return codigo;
+    return this.codigo;
+  }
+
+  public int getPosicao() {
+    return posicao;
   }
 
   public int getTotalRun() {
     return totalRun;
-  }
-
-  public boolean estaLigado() {
-    
-    if (estado == 1)
-      return true;
-
-    return false;
   }
 
   public boolean estaDesligado() {
--- Main.java
+++ Main.java
@@ -1,5 +1,35 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+      Elevador e = new Elevador();
+
+      e.inicializar("TORRE-B");
+
+    System.out.println("Nome: " + e.getCodigo() + " [rodagem: "+e.getTotalRun()+"]");
+      System.out.println("Andar: " + e.getPosicao());
+
+      e.mover(10);
+
+    System.out.println("Nome: " + e.getCodigo() + " [rodagem: "+e.getTotalRun()+"]");
+      System.out.println("Andar: " + e.getPosicao());
+
+      e.mover(3);
+
+    System.out.println("Nome: " + e.getCodigo() + " [rodagem: "+e.getTotalRun()+"]");
+      System.out.println("Andar: " + e.getPosicao());
+
+      e.mover(0);
+
+      e.desligar();
+
+    System.out.println("Nome: " + e.getCodigo() + " [rodagem: "+e.getTotalRun()+"]");
+      System.out.println("Andar: " + e.getPosicao());
+
+      e.inicializar("NEW-TOWER-CENTRAL");
+
+    System.out.println("Nome: " + e.getCodigo() + " [rodagem: "+e.getTotalRun()+"]");
+      System.out.println("Andar: " + e.getPosicao());
+
+    System.out.println("fim do teste!");
   }
 }
